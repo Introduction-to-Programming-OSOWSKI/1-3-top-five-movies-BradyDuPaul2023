3621b2094b252a33a80c6a9c0426f1ea7caf7a07
Dupaul Braedon <816947@stma.k12.mn.us>
2021-03-26 09:32:37 -0500

    Top five movies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def topFiveMovies(a, b, c, d, e):
-    return a + b + c + d + e
+    return a + " " + b + " " + c + " " + d + " " + e
 
-print(topFiveMovies("a ", "b ", "c ", "d ", "e"))
+print(topFiveMovies("a", "b", "c", "d", "e"))
